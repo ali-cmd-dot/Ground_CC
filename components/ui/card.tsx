09302af2788f22ed4df2ab1b0196793b29f2db1c
ali-cmd-dot <ali@cautio.in>
2026-02-15 17:40:52 +0530

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -46,7 +46,7 @@ CardTitle.displayName = "CardTitle"
 const CardDescription = React.forwardRef
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => (
+>(({ className, ...props}, ref) => (
   <p
     ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
