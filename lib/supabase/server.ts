fc2d073ab14bd144b02ffec238443ce1cd2ddee5
ali-cmd-dot <ali@cautio.in>
2026-02-16 19:16:53 +0530

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,7 +1,18 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export const createServerSupabaseClient = () => {
   const cookieStore = cookies()
-  return createServerComponentClient({ cookies: () => cookieStore })
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+      },
+    }
+  )
 }
